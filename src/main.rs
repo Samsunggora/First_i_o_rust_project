9521dcc1709bdb3fc3904398a51f15d5483e9c7f
flavik <Samsunggora@gmail.com>
2022-03-14 23:49:25 +0100

    More complex implementation via non-zero error output

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for: {}", config.query);
     println!("In file: {} \n", config.filename);
@@ -18,13 +22,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
-        if args.len()<3{
-            panic!("not enough arguments!")
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
