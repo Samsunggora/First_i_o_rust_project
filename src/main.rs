4d62b1752be9db826b8866cfe04331088b0f19cc
flavik <Samsunggora@gmail.com>
2022-03-15 00:07:47 +0100

    I split the logic into 2 different files, made the components used public.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use cla::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,31 +10,13 @@ fn main() {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    println!("Searching for: {}", config.query);
-    println!("In file: {} \n", config.filename);
 
-    run(config);
-}
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
+    if let Err(e) = cla::run(config) {
+        println!("Application error: {}", e);
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-    println!("With text: \n {}", contents)
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+        process::exit(1);
     }
 }
