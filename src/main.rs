77049d162cbfa3bf40073da3c3434e0103e30c74
flavik <Samsunggora@gmail.com>
2022-03-14 23:58:26 +0100

    I take out the functionality from the main function for easier writing of tests

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,21 @@ use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err|{
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-
     println!("Searching for: {}", config.query);
     println!("In file: {} \n", config.filename);
 
+    run(config);
+}
+
+
+fn run(config: Config) {
     let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents);
+    println!("With text: \n {}", contents)
 }
 
 struct Config {
